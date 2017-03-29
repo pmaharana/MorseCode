bc5c0f5c30d26e7638bbd106c4952549de8883a5
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-29 14:03:01 -0400

    --first commit, trying to read code

---
+++ MorseCode/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class Program
+    {
+        
+
+        
+
+
+        static void Main(string[] args)
+        {
+            const string morsePath = "morse.csv";
+
+            var morseKey = new List<Translator>();
+
+            using (var reader = new StreamReader(morsePath))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var code = reader.ReadLine();
+                    var split = code.Split(',');
+                    var translator = new Translator(split);
+                    morseKey.Add(translator);
+                }
+            }
+
+            foreach (var item in morseKey)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+           
+
+
+
+
+
+        }
+    }
+}
---
+++ MorseCode/Translator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    public class Translator
+    {
+        public string Key { get; set; }
+        public string Code { get; set; }
+        public override string ToString()
+        {
+            return $"{Key},{Code}";
+        }
+        public Translator()
+        {
+
+        }
+        public Translator(string[] data)
+        {
+            Key = data[0];
+            Code = data[1];
+        }
+
+    }
+}
